5f67a39a90c054c96839bd664d2915a44853eed3
Alexis Brandner <alexinfos@free.fr>
2025-04-09 09:31:46 +0200

    feat(redirect): Implement login redirection using GET param

--- src/protect/login.php
+++ src/protect/login.php
@@ -1,8 +1,6 @@
 <?php
 session_start();
 
-header('Content-Type: application/json; charset=utf-8');
-
 require_once __DIR__ . '/../../php/vendor/autoload.php';
 require_once __DIR__ . '/src/LoginResponse.php';
 require_once __DIR__ . '/../../php/auth-config.php';
@@ -15,9 +13,20 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+$redirect = null;
+if (isset($_SESSION['protect_redirect'])) {
+    $redirect = $_SESSION['protect_redirect'];
+}
+if (isset($_GET['redirect'])) {
+    $redirect = $_GET['redirect'];
+    $_SESSION['protect_redirect'] = $redirect;
+}
+
+
 if (isset($_SESSION['cas_data']) && $_SESSION['cas_data'] != "") {
     $response = new LoginResponse(TRUE, "User already connected", $_SESSION);
-    echo $response->toString();
+    $response->send($redirect);
+    $_SESSION['protect_redirect'] = null;
     exit();
 }
 
@@ -34,7 +43,8 @@ try {
     $openIdClient->authenticate();
 } catch (Jumbojett\OpenIDConnectClientException $e) {
     $response = new LoginResponse(FALSE, $e->getMessage(), $_SESSION);
-    echo $response->toString();
+    $response->send($redirect);
+    $_SESSION['protect_redirect'] = null;
     exit(0);
 }
 
@@ -43,19 +53,22 @@ try {
     $userInfo = $openIdClient->requestUserInfo();
 } catch (Jumbojett\OpenIDConnectClientException $e) {
     $response = new LoginResponse(FALSE, $e->getMessage(), $_SESSION);
-    echo $response->toString();
+    $response->send($redirect);
+    $_SESSION['protect_redirect'] = null;
     exit(0);
 }
 
 if (!isset($userInfo->uid)) {
     $response = new LoginResponse(FALSE, "Login failed for unknown reason", $_SESSION);
-    echo $response->toString();
+    $response->send($redirect);
+    $_SESSION['protect_redirect'] = null;
     exit(0);
 }
 
 $_SESSION['cas_data'] = $userInfo;
 
 $response = new LoginResponse(TRUE, "Login successful", $_SESSION);
-echo $response->toString();
+$response->send($redirect);
+$_SESSION['protect_redirect'] = null;
 
 ?>
