e28795a2b04a85885ff76dd4b22c3e84b772fec0
Nemo D'ACREMONT <ndacremont@enseirb-matmeca.fr>
2025-04-13 18:58:42 +0200

    fix: typo in location header

--- src/protect/logout.php
+++ src/protect/logout.php
@@ -7,8 +7,8 @@ session_unset();
 session_destroy();
 
 if (isset($_GET['redirect'])) {
-    header('Location :' . $_GET['redirect']);
+    header('Location: ' . $_GET['redirect']);
 } else {
-    header('Location : /');
+    header('Location: /');
 }
 ?>
