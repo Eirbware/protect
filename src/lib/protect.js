b58000ba3f7e1d4ba4d21df5fd4715e38fc229f8
Nemo D'ACREMONT <ndacremont@enseirb-matmeca.fr>
2025-04-13 18:58:00 +0200

    feat: add a demo example and add the js lib code

--- src/lib/protect.js
+++ src/lib/protect.js
@@ -1,11 +1,44 @@
-console.log("HelloWorld!");
-export default {
-    login: () => "salut",
-    getData: async () => {
-        if (Math.random() > 0.5)
-            throw new Error("dummy error");
+/*
+ * Redirect the browser to login page, login if required. After login, the
+ * browser redirects by default to the current URL.
+ */
+function login(redirect = window.location.origin + window.location.pathname) {
+    window.location = `${window.location.origin}/protect/login.php?redirect=${redirect}`;
+}
 
-        return { message: "empty" };
-    },
-    logout: () => "salut",
+
+/*
+ * Returns protected data and the user data from the user currently logged in
+ * @throws Error if the user is not currently logged in or if data fetching failed
+ * @returns {Promise<object>} json containing protected data and user data
+ * @example
+ * // returns {user: {uid: "ndacremont", etc: "..."}, protected: {video: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"}}
+ */
+async function getData() {
+    const response = await fetch("/protect/getData.php");
+
+    switch (response.status) {
+        case 200:
+            return response.json();
+        case 401:
+            throw new Error("Veuillez vous connecter dans un premier temps");
+        default:
+            throw new Error("Erreur inconnue, contactez un membre d'Eirbware")
+    }
+}
+
+
+/*
+ * Redirect the browser to logout page. After logout, the browser redirects by
+ * default to '/'.
+ */
+function logout(redirect = window.location.origin) {
+    window.location = `/protect/logout.php?redirect=${redirect}`;
+}
+
+
+export default {
+    login,
+    getData,
+    logout,
 };
