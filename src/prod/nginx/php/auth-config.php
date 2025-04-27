6f55e4c40877c46416dace0a54236d9f1a6d50a7
Nemo D'ACREMONT <ndacremont@enseirb-matmeca.fr>
2025-04-27 17:21:40 +0200

    feat: update prod env to latest changes

--- src/prod/nginx/php/auth-config.php
+++ src/prod/nginx/php/auth-config.php
@@ -10,13 +10,19 @@ $OPENID_CONFIG = [
     "server_url" => "https://connect.eirb.fr/realms/eirb",
     "client_id" => "CLIENT_ID",
     "client_secret" => "CLIENT_SECRET",
-    "redirect_url" => "REDIRECT_URL"
+];
+
+// Protected links accessible through `/protect/links.php?name=<link-name>` when authentified
+// URLs listed here must be valid according to RFC 2396 and start with http (or https)
+$PROTECTED_LINKS = [
+    "video" => "https://www.youtube.com/watch?v=xvFZjo5PgG0",
+    "url_pas_valide" => "example.org"
 ];
 
 // Protected data to return on site authentication (can be a string, an array..)
 $PROTECTED_DATA = [
     "message" =>"Je suis protégé !",
-    "video" => "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-    "theme_du_wei" => "wei.eirb.fr"
+    "video" => "https://www.youtube.com/watch?v=xvFZjo5PgG0",
+    "theme_du_wei" => "https://wei.eirb.fr"
 ];
 ?>
